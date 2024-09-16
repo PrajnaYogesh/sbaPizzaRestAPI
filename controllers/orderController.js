f7cca3955a825d585228968a26b43f91ee8ef1e2
Prajna <prajna.yogish4@gmail.com>
2024-09-16 17:59:32 -0400

    order complete

--- controllers/orderController.js
+++ controllers/orderController.js
@@ -15,4 +15,51 @@ const createOrder = async(req,res)=>{
     
 }
 
-module.exports = {createOrder};
+const getAllOrder = async(req,res)=>{
+    const allOrder = await Order.find();
+    res.json({Orders: allOrder});
+}
+
+const getAOrder = async(req,res)=>{
+    const orderID= req.params.id;
+    const order = await Order.findById(orderID);
+    if(!order){
+res.send({message : "No order Placed"})
+    }
+    res.json({Order: order});
+}
+
+
+
+const editOrder = async(req,res)=>{
+    const orderID= req.params.id;
+    const {orderNumber,
+    customerName,
+    pizza, 
+    total ,
+    orderTime} = req.body;
+
+await Order.findByIdAndUpdate(orderID,{
+    orderNumber:orderNumber,
+    customerName:customerName,
+    pizza:pizza, 
+    total:total ,
+    orderTime:orderTime
+})
+
+const updatedOrder = await Order.findById(orderID)
+res.json({Order : updatedOrder})
+
+
+}
+
+
+const deleteOrder= async(req,res) =>{
+    const orderID = req.params.id
+    await Order.findByIdAndDelete(orderID)
+    const order = await Order.find();
+    res.json({Order:order})
+    }
+    
+
+module.exports = {createOrder,getAllOrder,getAOrder,editOrder,deleteOrder};
--- routes/orderRoute.js
+++ routes/orderRoute.js
@@ -4,9 +4,9 @@ const order = require('../controllers/orderController')
 
 
 router.post('/add',order.createOrder);
-// router.get('/',order.getAllOrder)
-// router.get('/:id',order.getAOrder)
-// router.put('/:id',order.editOrder);
-// router.delete('/:id',order.deleteOrder)
+ router.get('/',order.getAllOrder)
+router.get('/:id',order.getAOrder)
+router.put('/:id',order.editOrder);
+router.delete('/:id',order.deleteOrder)
 
 module.exports = router;
